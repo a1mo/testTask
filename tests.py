6a944157f8527fb78edb0ae4851a6d9fb6e5cecc
a1m0 <frozenmorn@gmail.com>
2018-09-17 00:10:53 +0300

    remove line separator from test data

--- tests.py
+++ tests.py
@@ -6,13 +6,13 @@ class FinderTests(unittest.TestCase):
 
     def testGetLinesByRegExp(self):
         finder = Finder("\[(.*?)\]")
-        data = ["some text with [special] words\n", "in [square] brackets\n", "separated by lines"]
+        data = ["some text with [special] words", "in [square] brackets", "separated by lines"]
         processedData = finder.processData(data)
         self.assertEqual(data[:2], processedData)
 
     def testGetUniqueWords(self):
         finder = Finder()
-        data = ["text, without.\n", "meaning! with? repetitive words\n", "in words without meaning?"]
-        uniqueWords = ["text\n", "with\n", "repetitive\n", "in\n"]
+        data = ["text, without.", "meaning! with? repetitive words", "in words without meaning?"]
+        uniqueWords = ["text", "with", "repetitive", "in"]
         processedData = finder.processData(data)
         self.assertEqual(sorted(uniqueWords), sorted(processedData))
