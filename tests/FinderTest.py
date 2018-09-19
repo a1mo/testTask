23476250aa57d9b7a2d760eb09af0f566125a62a
a1m0 <frozenmorn@gmail.com>
2018-09-19 12:30:27 +0300

    rename tests func to more readable

--- tests/FinderTest.py
+++ tests/FinderTest.py
@@ -4,13 +4,13 @@ from libs.Finder import Finder
 
 class FinderTest(unittest.TestCase):
 
-    def testGetLinesByRegExp(self):
+    def testFinderReturnsMatchedLinesOnRegExpPassed(self):
         finder = Finder("\[(.*?)\]")
         expectedResult = ["some text with [special] words", "in [square] brackets"]
         processedData = finder.processData(expectedResult + ["separated by lines"])
         self.assertEqual(expectedResult, processedData)
 
-    def testGetUniqueWords(self):
+    def testFinderReturnsUniqueWordsWithoutRegExpPassed(self):
         finder = Finder()
         data = ["text, without.", "meaning! with? repetitive words", "in words without meaning?"]
         uniqueWords = ["text", "with", "repetitive", "in"]
